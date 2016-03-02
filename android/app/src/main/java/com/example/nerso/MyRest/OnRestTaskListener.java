4c5308ea902ba7d6b5acdad364423b54c6a32a39
Nelson Guimaraes <nelsonguimajunior@gmail.com>
2016-03-02 16:26:56 -0300

    final version commit (reverted from commit fafca4abd719329b20e05a7adb88cd0127a131e6)

--- android/app/src/main/java/com/example/nerso/MyRest/OnRestTaskListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.nerso.MyRest;
-
-public interface OnRestTaskListener {
-
-    public void onPreExecute();
-    public void onSuccess(String result);
-    public void onError(int responseCode, String message);
-
-}
